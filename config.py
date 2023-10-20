3037efd4831f5b20f4d8274f3dd6dce4801cf560
Ankush <ankushgt25@gmail.com>
2023-10-20 21:02:44 +0530

    updation

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # config.py
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'adminroot@123'
+# MYSQL_PASSWORD = 'adminroot@123'
+MYSQL_PASSWORD='ankushtiwari'
 MYSQL_DB = 'fluentfusion'
