40cdc45b1268dec6748b1f8328a0f3138f893fdc
Nikita Choudhari <choudharinikita96@gmail.com>
2023-10-28 23:20:20 +0530

    removed send email code

--- app/homepage.py
+++ app/homepage.py
@@ -2,9 +2,6 @@ import mysql.connector
 from flask import Flask, Blueprint, request, render_template
 from config import MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB
 import hashlib
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 
 # Create a Blueprint for this route
 homepage_app = Blueprint('homepage', __name__)
@@ -88,53 +85,10 @@ def register():
         if password != confirm_password:
             return "Passwords do not match."
 
-        # Add more complex validation as per your requirements
-
         # Add the user if validation passes
         add_user(name, email, password, country)
-        send_email(email)
         print("User details added to the database.")
         return "User successfully registered."
-
-
-def send_email(user_email):
-    sender_email = "your_email@gmail.com"  # Your email
-    receiver_email = user_email
-    password = "your_password"  # Your email account password
-
-    message = MIMEMultipart("alternative")
-    message["Subject"] = "Registration Successful"
-    message["From"] = sender_email
-    message["To"] = receiver_email
-
-    # Create the plain-text and HTML version of your message
-    text = """\
-    Hi,
-    Thank you for registering."""
-
-    html = """\
-    <html>
-      <body>
-        <p>Hi,<br>
-           Thank you for registering.
-        </p>
-      </body>
-    </html>
-    """
-
-    # Turn these into plain/html MIMEText objects
-    part1 = MIMEText(text, "plain")
-    part2 = MIMEText(html, "html")
-
-    # Add HTML/plain-text parts to MIMEMultipart message
-    # The email client will try to render the last part first
-    message.attach(part1)
-    message.attach(part2)
-
-    # Create secure connection with server and send email
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
 
 
 if __name__ == '__main__':
