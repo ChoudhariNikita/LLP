71db98238a8803075967da5d81a0939b05e76b4c
Nikita Choudhari <choudharinikita96@gmail.com>
2023-10-30 22:20:31 +0530

    Database creation commands used

--- config.py
+++ config.py
@@ -4,28 +4,30 @@ MYSQL_USER = 'root'
 MYSQL_PASSWORD = 'adminroot@123'
 MYSQL_DB = 'fluentfusion'
 
-# CREATE TABLE user (
-#     id INT AUTO_INCREMENT PRIMARY KEY,
-#     name VARCHAR(255) NOT NULL,
-#     email VARCHAR(255) NOT NULL,
-#     password VARCHAR(255) NOT NULL,
-#     country VARCHAR(255) NOT NULL
-# );
+'''
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL
+);
 
-# CREATE TABLE course (
-#     course_id INT AUTO_INCREMENT PRIMARY KEY,
-#     title VARCHAR(255),
-#     description TEXT,
-#     teacher_id INT,
-#     FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id)
-# );
+CREATE TABLE course (
+    course_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    description TEXT,
+    teacher_id INT,
+    FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id)
+);
 
-# CREATE TABLE teacher (
-#     teacher_id INT AUTO_INCREMENT PRIMARY KEY,
-#     teacher_name VARCHAR(255),
-#     qualification VARCHAR(255),
-#     major_subject VARCHAR(255),
-#     ratings FLOAT
-# );
+CREATE TABLE teacher (
+    teacher_id INT AUTO_INCREMENT PRIMARY KEY,
+    teacher_name VARCHAR(255),
+    qualification VARCHAR(255),
+    major_subject VARCHAR(255),
+    ratings FLOAT
+);
 
-# ALTER TABLE course ADD FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id);
+ALTER TABLE course ADD FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id);
+'''
--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from flask import Flask, Blueprint
 
 app = Flask(__name__)
 
-app.secret_key = 'your secret key'
+app.secret_key = 'Fluentfusion123'
 
 app.register_blueprint(homepage_app)
 app.register_blueprint(user_dashboard_app)
